a1ec5350f9fd1c6430c6ae6934d4448bc705dbb8
Brian Scott <brhinescot@users.noreply.github.com>
2007-07-09 04:20:07 +0000

    Fix for workitem 2730 - Csc.exe stopping windows from shutting down properly when cropper is running.

--- src/Cropper.Core/HotKeySelection.Designer.cs
+++ src/Cropper.Core/HotKeySelection.Designer.cs
@@ -29,55 +29,57 @@ namespace Fusion8.Cropper.Core
         private void InitializeComponent()
         {
             System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-                                                                                                                     "Show",
-                                                                                                                     "",
-                                                                                                                     "Yes"}, -1);
+            "Show",
+            "",
+            "Yes"}, -1);
             System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-                                                                                                                     "Hide",
-                                                                                                                     "",
-                                                                                                                     "No"}, -1);
+            "Hide",
+            "",
+            "No"}, -1);
             System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
-                                                                                                                     "Thumbnail",
-                                                                                                                     "",
-                                                                                                                     "No"}, -1);
+            "Thumbnail",
+            "",
+            "No"}, -1);
             System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
-                                                                                                                     "Next Size",
-                                                                                                                     "",
-                                                                                                                     "No"}, -1);
+            "Next Size",
+            "",
+            "No"}, -1);
             System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
-                                                                                                                     "Capture",
-                                                                                                                     "",
-                                                                                                                     "Yes"}, -1);
+            "Capture",
+            "",
+            "Yes"}, -1);
             this.shortcutList = new System.Windows.Forms.ListView();
             this.actionHeader = new System.Windows.Forms.ColumnHeader();
             this.shortcutHeader = new System.Windows.Forms.ColumnHeader();
             this.globalHeader = new System.Windows.Forms.ColumnHeader();
             this.shortcutAssign = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.shortcut = new Fusion8.Cropper.Core.ShortcutTextBox();
             this.SuspendLayout();
             // 
             // shortcutList
             // 
             this.shortcutList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                                                                              | System.Windows.Forms.AnchorStyles.Left)
-                                                                             | System.Windows.Forms.AnchorStyles.Right)));
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.shortcutList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-                                                                                           this.actionHeader,
-                                                                                           this.shortcutHeader,
-                                                                                           this.globalHeader});
+            this.actionHeader,
+            this.shortcutHeader,
+            this.globalHeader});
             this.shortcutList.FullRowSelect = true;
             this.shortcutList.GridLines = true;
             this.shortcutList.HideSelection = false;
             this.shortcutList.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-                                                                                         listViewItem1,
-                                                                                         listViewItem2,
-                                                                                         listViewItem3,
-                                                                                         listViewItem4,
-                                                                                         listViewItem5});
-            this.shortcutList.Location = new System.Drawing.Point(3, 3);
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5});
+            this.shortcutList.Location = new System.Drawing.Point(3, 16);
             this.shortcutList.Name = "shortcutList";
-            this.shortcutList.Size = new System.Drawing.Size(261, 115);
-            this.shortcutList.TabIndex = 5;
+            this.shortcutList.Size = new System.Drawing.Size(313, 198);
+            this.shortcutList.TabIndex = 1;
             this.shortcutList.UseCompatibleStateImageBehavior = false;
             this.shortcutList.View = System.Windows.Forms.View.Details;
             this.shortcutList.SelectedIndexChanged += new System.EventHandler(this.HandleShortcutListSelectedIndexChanged);
@@ -100,32 +102,53 @@ namespace Fusion8.Cropper.Core
             // shortcutAssign
             // 
             this.shortcutAssign.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.shortcutAssign.Location = new System.Drawing.Point(189, 123);
+            this.shortcutAssign.Location = new System.Drawing.Point(241, 231);
             this.shortcutAssign.Name = "shortcutAssign";
             this.shortcutAssign.Size = new System.Drawing.Size(75, 22);
             this.shortcutAssign.TabIndex = 4;
-            this.shortcutAssign.Text = "Assign";
+            this.shortcutAssign.Text = "&Assign";
             this.shortcutAssign.UseVisualStyleBackColor = true;
             this.shortcutAssign.Click += new System.EventHandler(this.HandleShorcutAssignClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hot Key &List";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hot &Key";
             // 
             // shortcut
             // 
             this.shortcut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                                                                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.shortcut.Location = new System.Drawing.Point(3, 125);
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.shortcut.Location = new System.Drawing.Point(3, 233);
             this.shortcut.Name = "shortcut";
-            this.shortcut.Size = new System.Drawing.Size(180, 20);
+            this.shortcut.Size = new System.Drawing.Size(232, 20);
             this.shortcut.TabIndex = 3;
             // 
             // HotKeySelection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.shortcutList);
             this.Controls.Add(this.shortcutAssign);
             this.Controls.Add(this.shortcut);
             this.Name = "HotKeySelection";
-            this.Size = new System.Drawing.Size(267, 152);
+            this.Size = new System.Drawing.Size(319, 260);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,5 +162,7 @@ namespace Fusion8.Cropper.Core
         private System.Windows.Forms.ColumnHeader globalHeader;
         private System.Windows.Forms.Button shortcutAssign;
         private Fusion8.Cropper.Core.ShortcutTextBox shortcut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
