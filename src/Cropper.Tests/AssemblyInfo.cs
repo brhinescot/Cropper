6429d3a9c10562404b38400d31c84ec23c5aa6cb
TerryHughes_cp <SND\TerryHughes_cp>
2009-11-10 04:38:25 +0000

    Added a test project

---
+++ src/Cropper.Tests/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Security.Permissions;
+[assembly : SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode=false)]
+[assembly : AssemblyTitle("Cropper.Tests")]
+[assembly : AssemblyDefaultAlias("Cropper.Tests")]
+[assembly : AssemblyDescription("Cropper.Tests")]
+[assembly : AssemblyConfiguration("Cropper")]
+[assembly : AssemblyCompany("Fusion8 Design")]
+[assembly : AssemblyProduct("Cropper")]
+[assembly : AssemblyCopyright("Copyright 2004 Brian Scott (Fusion8 Design). All rights reserved.")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+[assembly : AssemblyVersion("1.0.0.0")]
+[assembly : NeutralResourcesLanguage("en-US")]
