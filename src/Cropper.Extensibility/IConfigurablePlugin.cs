a5322ddde73a9efa0c6722af2239d82e1c301cb3
TerryHughes_cp <SND\TerryHughes_cp>
2009-11-19 06:08:22 +0000

    * src/Cropper.Extensibility/IConfigurablePlugin.cs: Removed hard line breaks in XML comments

--- src/Cropper.Extensibility/IConfigurablePlugin.cs
+++ src/Cropper.Extensibility/IConfigurablePlugin.cs
@@ -63,14 +63,12 @@ namespace Fusion8.Cropper.Extensibility
     public interface IConfigurablePlugin : IPersistableImageFormat
     {
         /// <summary>
-        /// Gets the plug-in's impementation of the <see cref="BaseConfigurationForm"/> used 
-        /// for setting plug-in specific options.
+        /// Gets the plug-in's impementation of the <see cref="BaseConfigurationForm" /> used for setting plug-in specific options.
         /// </summary>
         BaseConfigurationForm ConfigurationForm { get; }
 
         /// <summary>
-        /// Gets a value indicating if the <see cref="ConfigurationForm"/> is to be hosted 
-        /// in the options dialog or shown in its own dialog window.
+        /// Gets a value indicating if the <see cref="ConfigurationForm" /> is to be hosted in the options dialog or shown in its own dialog window.
         /// </summary>
         bool HostInOptions { get; }
 
@@ -78,12 +76,9 @@ namespace Fusion8.Cropper.Extensibility
         /// Gets or sets an object containing the plug-in's settings.
         /// </summary>
         /// <remarks>
-        /// <para>
-        /// This property is set during startup with the settings contained in the applications
-        /// configuration file.</para>
-        /// <para>
-        /// The object returned by this property is serialized into the applications configuration
-        /// file on shutdown.</para></remarks>
+        /// <para>This property is set during startup with the settings contained in the applications configuration file.</para>
+        /// <para>The object returned by this property is serialized into the applications configuration file on shutdown.</para>
+        /// </remarks>
         object Settings { get; set; }
     }
 }
