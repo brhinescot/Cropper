402a2beb9d19486f5cb8400d85054b07f64180b1
TerryHughes_cp <SND\TerryHughes_cp>
2009-11-20 06:14:28 +0000

    * src/Cropper.Core/Templates.cs: Switched indentation to spaces

--- src/Cropper.Core/Templates.cs
+++ src/Cropper.Core/Templates.cs
@@ -57,19 +57,19 @@ In return, we simply require that you agree:
 
 namespace Fusion8.Cropper.Core
 {
-	/// <summary>
-	/// Summary description for Templates.
-	/// </summary>
-	internal struct Templates
-	{
-		internal const string Date = "{date}";
-		internal const string Time = "{time}";
-		internal const string Extension = "{extension}";
-		internal const string Increment = "{increment}";
-		internal const string User = "{user}";
-		internal const string Domain = "{domain}";
-		internal const string Machine = "{machine}";
-		internal const string Prompt = "{prompt}";
-		internal const string Window = "{window}";
-	}
+    /// <summary>
+    /// Summary description for Templates.
+    /// </summary>
+    internal struct Templates
+    {
+        internal const string Date = "{date}";
+        internal const string Time = "{time}";
+        internal const string Extension = "{extension}";
+        internal const string Increment = "{increment}";
+        internal const string User = "{user}";
+        internal const string Domain = "{domain}";
+        internal const string Machine = "{machine}";
+        internal const string Prompt = "{prompt}";
+        internal const string Window = "{window}";
+    }
 }
