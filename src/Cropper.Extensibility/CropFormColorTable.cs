a914194a0f6b493a847b74c2f165af2d69286225
Terry Hughes <terry@tnthughes.net>
2009-11-23 08:20:35 +0000

    * src/Cropper.Extensibility/CropFormColorTable.cs: Switched indentation to spaces

--- src/Cropper.Extensibility/CropFormColorTable.cs
+++ src/Cropper.Extensibility/CropFormColorTable.cs
@@ -2,40 +2,40 @@ using System.Drawing;
 
 namespace Fusion8.Cropper.Extensibility
 {
-	/// <summary>
-	/// Summary description for CropFormColorTable.
-	/// </summary>
-	public abstract class CropFormColorTable
-	{
-		private int mainAlphaChannel;
-		private int tabAlphaChannel = 200;
-		
-		public virtual bool SupportsPerPixelAlpha
-		{
-			get { return true; }
-		}
-		
-		public virtual int MainAlphaChannel
-		{
-			get { return mainAlphaChannel; }
-			set { mainAlphaChannel = value; }
-		}
+    /// <summary>
+    /// Summary description for CropFormColorTable.
+    /// </summary>
+    public abstract class CropFormColorTable
+    {
+        private int mainAlphaChannel;
+        private int tabAlphaChannel = 200;
 
-		public int TabAlphaChannel
-		{
-			get { return tabAlphaChannel; }
-			set { tabAlphaChannel = value; }
-		}
+        public virtual bool SupportsPerPixelAlpha
+        {
+            get { return true; }
+        }
 
-		public abstract Color TabColor { get; }
-		public abstract Color TabHighlightColor { get; }
-		public abstract Color TabTextColor { get; }
-		public abstract Color TabTextHighlightColor { get; }
-		public abstract Color FormColor { get; }
-		public abstract Color FormHighlightColor { get; }
-		public abstract Color FormTextColor { get; }
-		public abstract Color FormTextHighlightColor { get; }
-		public abstract Color LineColor { get; }
-		public abstract Color LineHighlightColor { get; }
-	}
+        public virtual int MainAlphaChannel
+        {
+            get { return mainAlphaChannel; }
+            set { mainAlphaChannel = value; }
+        }
+
+        public int TabAlphaChannel
+        {
+            get { return tabAlphaChannel; }
+            set { tabAlphaChannel = value; }
+        }
+
+        public abstract Color TabColor { get; }
+        public abstract Color TabHighlightColor { get; }
+        public abstract Color TabTextColor { get; }
+        public abstract Color TabTextHighlightColor { get; }
+        public abstract Color FormColor { get; }
+        public abstract Color FormHighlightColor { get; }
+        public abstract Color FormTextColor { get; }
+        public abstract Color FormTextHighlightColor { get; }
+        public abstract Color LineColor { get; }
+        public abstract Color LineHighlightColor { get; }
+    }
 }
