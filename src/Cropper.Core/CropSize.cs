15ce2c25601b36b64fe265aac03da1234b4e7b7a
Terry Hughes <terry@tnthughes.net>
2009-11-24 04:01:19 +0000

    * src/Cropper.Core/CropSize.cs: Updated references to private variables width and height to their respective properties   (Width): Converted to automatic property   (Height): Converted to automatic property
    * src/Cropper.Tests/Core/CropSizeTests.cs
      (Width_is_0_by_default, Width_property_is_not_altered, Height_is_0_by_default, Height_property_is_not_altered): Removed

--- src/Cropper.Core/CropSize.cs
+++ src/Cropper.Core/CropSize.cs
@@ -61,52 +61,41 @@ namespace Fusion8.Cropper.Core
 {
     public struct CropSize : IEquatable<CropSize>, IComparable<CropSize>
     {
-        private int width;
-        private int height;
+        public int Width { get; set; }
 
-        public int Width
-        {
-            get { return width; }
-            set { width = value; }
-        }
+        public int Height { get; set; }
 
-        public int Height
+        public CropSize(int width, int height) : this()
         {
-            get { return height; }
-            set { height = value; }
-        }
-
-        public CropSize(int width, int height)
-        {
-            this.width = width;
-            this.height = height;
+            this.Width = width;
+            this.Height = height;
         }
 
         public override string ToString()
         {
-            return string.Format("{0} x {1}", width, height);
+            return string.Format("{0} x {1}", Width, Height);
         }
 
         public bool Equals(CropSize other)
         {
-            return (other.Width == Width && other.Height == Height) ;
+            return other.Width == Width && other.Height == Height;
         }
 
         public int CompareTo(CropSize other)
         {
-            return (Width - other.Width != 0) ? (Width - other.Width) : (Height - other.Height);
+            return Width - other.Width != 0 ? Width - other.Width : Height - other.Height;
         }
 
         public override bool Equals(object obj)
         {
             if (!(obj is CropSize)) return false;
             CropSize cropSize = (CropSize) obj;
-            return width == cropSize.width && height == cropSize.height;
+            return Width == cropSize.Width && Height == cropSize.Height;
         }
 
         public override int GetHashCode()
         {
-            return width + 29*height;
+            return Width + 29 * Height;
         }
     }
 }
