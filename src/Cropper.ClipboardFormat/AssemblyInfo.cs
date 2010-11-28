6605db74e3b4dd50c49ee983057805db02bc91c4
Terry Hughes <terry@tnthughes.net>
2010-11-28 05:35:08 +0000

    Updated the version numbers for v1.9.4

--- src/Cropper.ClipboardFormat/AssemblyInfo.cs
+++ src/Cropper.ClipboardFormat/AssemblyInfo.cs
@@ -68,5 +68,5 @@ using System.Security.Permissions;
 [assembly : AssemblyCopyright("Copyright 2004 Brian Scott (Fusion8 Design). All rights reserved.")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
-[assembly : AssemblyVersion("1.2.1.0")]
+[assembly : AssemblyVersion("1.2.2.0")]
 [assembly : NeutralResourcesLanguage("en-US")]
